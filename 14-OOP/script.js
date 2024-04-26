f60a48863727fe226d8813ca00abc44cfa9143f7
akshay agnihotri <agnihotriakshay36@gmail.com>
2024-04-26 21:42:59 +0530

    object.create way o creating a object

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -207,3 +207,28 @@ console.log(jessica.fullName);
 
 const walter = new PersonCl('Walter White', 1965);
 PersonCl.hey(); //hey is a static method so it this keyword will point to whole class
+
+///////////////////////////////////////
+// Object.create
+const PersonProto = {
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  },
+
+  init(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  },
+};
+
+const steven = Object.create(PersonProto);
+console.log(steven);
+steven.name = 'Steven';
+steven.birthYear = 2002;
+steven.calcAge();
+
+console.log(steven.__proto__ === PersonProto);
+
+const sarah = Object.create(PersonProto);
+sarah.init('Sarah', 1979);
+sarah.calcAge();
