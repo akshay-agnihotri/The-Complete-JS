9512b7dfb01626234ac8247778c90e10f92d1b3b
akshay agnihotri <agnihotriakshay36@gmail.com>
2024-04-26 18:07:55 +0530

    completed coding challenge 1

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -1,6 +1,5 @@
 'use strict';
 
-
 ///////////////////////////////////////
 // Constructor Functions and the new Operator
 const Person = function (firstName, birthYear) {
@@ -28,58 +27,93 @@ const jack = new Person('Jack', 1975);
 console.log(jonas instanceof Person);
 
 Person.hey = function () {
-    console.log('Hey there 👋');
-    console.log(this);
-  };
-  Person.hey();
-  
-  ///////////////////////////////////////
-  // Prototypes
-  console.log(Person.prototype);
-  
-  Person.prototype.calcAge = function () {
-    console.log(2037 - this.birthYear);
-  };
-  
-  jonas.calcAge();
-  matilda.calcAge();
-  
-  console.log(jonas.__proto__);
-  console.log(jonas.__proto__ === Person.prototype);
-  
-  console.log(Person.prototype.isPrototypeOf(jonas));
-  console.log(Person.prototype.isPrototypeOf(matilda));
-  console.log(Person.prototype.isPrototypeOf(Person));
-  
-  // .prototyeOfLinkedObjects
-  
-  Person.prototype.species = 'Homo Sapiens';
-  console.log(jonas.species, matilda.species);
-  
-  console.log(jonas.hasOwnProperty('firstName'));
-  console.log(jonas.hasOwnProperty('species'));
-  
-  
-  ///////////////////////////////////////
-  // Prototypal Inheritance on Built-In Objects
-  console.log(jonas.__proto__);
-  // Object.prototype (top of prototype chain)
-  console.log(jonas.__proto__.__proto__);
-  console.log(jonas.__proto__.__proto__.__proto__);
-  
-  console.dir(Person.prototype.constructor);
-  
-  const arr = [3, 6, 6, 5, 6, 9, 9]; // new Array === []
-  console.log(arr.__proto__);
-  console.log(arr.__proto__ === Array.prototype);
-  
-  console.log(arr.__proto__.__proto__);
-  
-  Array.prototype.unique = function () {
-    return [...new Set(this)];
-  };
-  
-  console.log(arr.unique());
-  
-  const h1 = document.querySelector('h1');
-  console.dir(x => x + 1); 
+  console.log('Hey there 👋');
+  console.log(this);
+};
+Person.hey();
+
+///////////////////////////////////////
+// Prototypes
+console.log(Person.prototype);
+
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
+
+jonas.calcAge();
+matilda.calcAge();
+
+console.log(jonas.__proto__);
+console.log(jonas.__proto__ === Person.prototype);
+
+console.log(Person.prototype.isPrototypeOf(jonas));
+console.log(Person.prototype.isPrototypeOf(matilda));
+console.log(Person.prototype.isPrototypeOf(Person));
+
+// .prototyeOfLinkedObjects
+
+Person.prototype.species = 'Homo Sapiens';
+console.log(jonas.species, matilda.species);
+
+console.log(jonas.hasOwnProperty('firstName'));
+console.log(jonas.hasOwnProperty('species'));
+
+///////////////////////////////////////
+// Prototypal Inheritance on Built-In Objects
+console.log(jonas.__proto__);
+// Object.prototype (top of prototype chain)
+console.log(jonas.__proto__.__proto__);
+console.log(jonas.__proto__.__proto__.__proto__);
+
+console.dir(Person.prototype.constructor);
+
+const arr = [3, 6, 6, 5, 6, 9, 9]; // new Array === []
+console.log(arr.__proto__);
+console.log(arr.__proto__ === Array.prototype);
+
+console.log(arr.__proto__.__proto__);
+
+Array.prototype.unique = function () {
+  return [...new Set(this)];
+};
+
+console.log(arr.unique());
+
+const h1 = document.querySelector('h1');
+console.dir(x => x + 1);
+
+///////////////////////////////////////
+// Coding Challenge #1
+
+/* 
+    1. Use a constructor function to implement a Car. A car has a make and a speed property. The speed property is the current speed of the car in km/h;
+    2. Implement an 'accelerate' method that will increase the car's speed by 10, and log the new speed to the console;
+    3. Implement a 'brake' method that will decrease the car's speed by 5, and log the new speed to the console;
+    4. Create 2 car objects and experiment with calling 'accelerate' and 'brake' multiple times on each of them.
+
+    DATA CAR 1: 'BMW' going at 120 km/h
+    DATA CAR 2: 'Mercedes' going at 95 km/h
+
+    GOOD LUCK 😀
+*/
+
+const Car = function(make,currentSpeed){
+    this.make = make;
+    this.speed = `${parseInt(currentSpeed)}km/h`;
+}
+Car.prototype.accelerate = function(){
+    this.speed = `${parseInt(this.speed) + 10}km/h`;
+    console.log(this.speed);
+}
+Car.prototype.brake = function(){
+    this.speed = `${parseInt(this.speed) - 5}km/h`;
+    console.log(this.speed);
+}
+
+let car1 = new Car('BMW' , '120km/h');
+let car2 = new Car('Mercedes','95km/h');
+
+console.log(car1);
+car1.accelerate();
+car1.brake();
+console.log(car1);
