11b5b8ab6dd9c7ad5b190e8a9876f89e1a919647
akshay agnihotri <agnihotriakshay36@gmail.com>
2024-04-28 23:21:32 +0530

    adding map in the webpage

--- 15-Mapty/script.js
+++ 15-Mapty/script.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // prettier-ignore
-// const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
 const form = document.querySelector('.form');
 const containerWorkouts = document.querySelector('.workouts');
@@ -11,4 +11,28 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+const options = {
+  enableHighAccuracy: true,
+  timeout: 10000,
+  maximumAge: 0,
+};
 
+function success(pos) {
+  const { latitude, longitude } = pos.coords;
+  const map = L.map('map').setView([latitude, longitude], 13);
+
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  }).addTo(map);
+}
+
+function error(err) {
+  console.warn(`ERROR(${err.code}): ${err.message}`);
+}
+
+// ON load
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(success, error, options);
+}
