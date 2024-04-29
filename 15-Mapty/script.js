0d2f87d96cbb1000b298eee916e44a23cb71a150
akshay agnihotri <agnihotriakshay36@gmail.com>
2024-04-29 20:13:36 +0530

    Rendering Workout Input Form

--- 15-Mapty/script.js
+++ 15-Mapty/script.js
@@ -11,27 +11,10 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-// async function fetchReverseGeocodeData(lat, lng, marker) {
-//   const apiUrl = `https://api.geoapify.com/v1/geocode/reverse?lat=${lat}&lon=${lng}&apiKey=5b361b367a794584bbb337eab81113aa`;
-
-//   try {
-//     const response = await fetch(apiUrl);
-
-//     if (!response.ok) {
-//       throw new Error(`HTTP error! Status: ${response.status}`);
-//     }
-
-//     const data = await response.json();
-//     // console.log(data.features[0].properties.address_line1);
-//     marker
-//       .bindPopup(`<b>${data.features[0].properties.address_line2}</b>`)
-//       .openPopup();
-//   } catch (error) {
-//     console.error('Error fetching reverse geocode data:', error);
-//     // You can handle errors here, such as logging them or showing to the user
-//     return null; // or throw error if you want to handle it elsewhere
-//   }
-// }
+// variables which need to be global to add marker in map
+let map;
+let lat;
+let lng;
 
 const options = {
   enableHighAccuracy: true,
@@ -42,7 +25,7 @@ const options = {
 function success(pos) {
   // Making a simple map
   const { latitude, longitude } = pos.coords;
-  const map = L.map('map').setView([latitude, longitude], 14);
+  map = L.map('map').setView([latitude, longitude], 14);
 
   L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
@@ -50,40 +33,15 @@ function success(pos) {
       '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
   }).addTo(map);
 
-  // Adding a event listener to add Marker
-  function onMapClick(e) {
-    const { lat, lng } = e.latlng;
-    const marker = L.marker([lat, lng], {
-      draggable: true,
-      maxWidth: 300,
-      minWidth: 50,
-      className: 'running-popup',
-    }).addTo(map);
-
-    const popup = L.popup({
-      maxWidth: 300,
-      minWidth: 50,
-      autoClose: false,
-      closeOnEscapeKey: false,
-      className: 'running-popup',
-      closeOnClick: false,
-    })
-      .setLatLng([lat, lng])
-      .setContent('Running')
-      .openOn(map);
-    // fetchReverseGeocodeData(lat, lng, marker);
-
-    // marker.on('dragend', function (e) {
-    //   const { lat, lng } = e.target._latlng;
-
-    //   fetchReverseGeocodeData(lat, lng, marker);
-    // });
-    // marker.on('mouseover', function (e) {
-    //   console.log(e.sourceTarget);
-    // });
-  }
-
-  map.on('click', onMapClick);
+  // 1.As the user clicks on map we have to remove the hidden class from 'form';
+  // 2.As the user click on map then the focus on will be at the Distance input inside the form
+  // 3.add an Event Listener on submiting the form
+  map.on('click', function (e) {
+    lat = e.latlng.lat;
+    lng = e.latlng.lng;
+    form.classList.remove('hidden');
+    inputDistance.focus();
+  });
 }
 
 function error(err) {
@@ -94,3 +52,34 @@ function error(err) {
 if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(success, error, options);
 }
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  // 1.on submit the form we have to make the input field empty
+  inputDistance.value = inputDuration.value = inputCadence.value = '';
+  // 2.on selecting the type 'cycling' then the 'Cadence' will appear[line no.82]
+
+  const marker = L.marker([lat, lng], {
+    draggable: true,
+    maxWidth: 300,
+    minWidth: 50,
+    className: 'running-popup',
+  }).addTo(map);
+
+  const popup = L.popup({
+    maxWidth: 300,
+    minWidth: 50,
+    autoClose: false,
+    closeOnEscapeKey: false,
+    className: 'running-popup',
+    closeOnClick: false,
+  })
+    .setLatLng([lat, lng])
+    .setContent('Running')
+    .openOn(map);
+});
+
+inputType.addEventListener('change', function (e) {
+  inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+});
