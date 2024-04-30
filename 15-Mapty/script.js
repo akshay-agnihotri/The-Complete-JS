6b43a9650977ee37b1ab5033878933135626ba08
akshay agnihotri <agnihotriakshay36@gmail.com>
2024-04-30 19:47:52 +0530

    completed making the workout , running and cycling class

--- 15-Mapty/script.js
+++ 15-Mapty/script.js
@@ -11,75 +11,119 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-// variables which need to be global to add marker in map
-let map;
-let lat;
-let lng;
+class Workout {
+  date = new Date();
+  id = (Date.now() + '').slice(-10);
 
-const options = {
-  enableHighAccuracy: true,
-  timeout: 10000,
-  maximumAge: 0,
-};
+  constructor(distance, duration, coords) {
+    this.distance = distance; // in min
+    this.duration = duration; //in km
+    this.coords = coords; //[lat,lng]
+  }
+}
 
-function success(pos) {
-  // Making a simple map
-  const { latitude, longitude } = pos.coords;
-  map = L.map('map').setView([latitude, longitude], 14);
+class Running extends Workout {
+  constructor(distance, duration, coords, cadence) {
+    super(distance, duration, coords);
+    this.cadence = cadence;
+    this.calcPace();
+  }
 
-  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution:
-      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-  }).addTo(map);
+  calcPace() {
+    // min/km
+    this.pace = this.duration / this.distance;
+    return this.pace;
+  }
+}
 
-  // 1.As the user clicks on map we have to remove the hidden class from 'form';
-  // 2.As the user click on map then the focus on will be at the Distance input inside the form
-  // 3.add an Event Listener on submiting the form
-  map.on('click', function (e) {
-    lat = e.latlng.lat;
-    lng = e.latlng.lng;
+class Cycling extends Workout {
+  constructor(distance, duration, coords, elevationGain) {
+    super(distance, duration, coords);
+    this.elevationGain = elevationGain;
+    this.calcSpeed();
+  }
+  calcSpeed() {
+    // km/h
+    this.speed = this.distance / (this.duration / 60);
+    return this.speed;
+  }
+}
+
+class App {
+  #map;
+  #lat;
+  #lng;
+  constructor() {
+    // As the page loads then the constructor function will trigger => _getPosition() => _loadMap()
+    this._getPosition();
+    form.addEventListener('submit', this._newWorkOut.bind(this));
+    inputType.addEventListener('change', this._toggleElevationField);
+  }
+
+  _getPosition() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        this._loadMap.bind(this),
+        function error(err) {
+          console.warn(`ERROR(${err.code}): ${err.message}`);
+        }
+      );
+    }
+  }
+
+  _loadMap(pos) {
+    // Making a simple map
+    const { latitude, longitude } = pos.coords;
+    this.#map = L.map('map').setView([latitude, longitude], 14);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution:
+        '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    }).addTo(this.#map);
+    this.#map.on('click', this._showForm.bind(this));
+  }
+
+  _showForm(e) {
+    this.#lat = e.latlng.lat;
+    this.#lng = e.latlng.lng;
     form.classList.remove('hidden');
     inputDistance.focus();
-  });
+  }
+
+  _toggleElevationField() {
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+  }
+
+  _newWorkOut(e) {
+    e.preventDefault();
+    // 1.on submit the form we have to make the input field empty
+    inputDistance.value = inputDuration.value = inputCadence.value = '';
+    // 2.on selecting the type 'cycling' then the 'Cadence' will appear[line no.82]
+
+    const marker = L.marker([this.#lat, this.#lng], {
+      draggable: true,
+      maxWidth: 300,
+      minWidth: 50,
+      className: 'running-popup',
+    }).addTo(this.#map);
+
+    const popup = L.popup({
+      maxWidth: 300,
+      minWidth: 50,
+      autoClose: false,
+      closeOnEscapeKey: false,
+      className: 'running-popup',
+      closeOnClick: false,
+    })
+      .setLatLng([this.#lat, this.#lng])
+      .setContent('Running')
+      .openOn(this.#map);
+  }
 }
 
-function error(err) {
-  console.warn(`ERROR(${err.code}): ${err.message}`);
-}
+const app = new App();
 
-// ON load
-if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(success, error, options);
-}
 
-form.addEventListener('submit', function (e) {
-  e.preventDefault();
-  // 1.on submit the form we have to make the input field empty
-  inputDistance.value = inputDuration.value = inputCadence.value = '';
-  // 2.on selecting the type 'cycling' then the 'Cadence' will appear[line no.82]
 
-  const marker = L.marker([lat, lng], {
-    draggable: true,
-    maxWidth: 300,
-    minWidth: 50,
-    className: 'running-popup',
-  }).addTo(map);
-
-  const popup = L.popup({
-    maxWidth: 300,
-    minWidth: 50,
-    autoClose: false,
-    closeOnEscapeKey: false,
-    className: 'running-popup',
-    closeOnClick: false,
-  })
-    .setLatLng([lat, lng])
-    .setContent('Running')
-    .openOn(map);
-});
-
-inputType.addEventListener('change', function (e) {
-  inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
-  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
-});
