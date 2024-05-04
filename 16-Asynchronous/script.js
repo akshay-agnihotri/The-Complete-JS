7645b610b40c3914bb61ca6f5abbd5b49ae74672
akshay agnihotri <agnihotriakshay36@gmail.com>
2024-05-04 14:05:05 +0530

    callback hell

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -1,7 +1,9 @@
 'use strict';
 
+///////////////////////////////////////
+// Our First AJAX Call: XMLHttpRequest
+/*
 const  countriesContainer = document.querySelector(".countries");
-
 
 const getCountryData = function (country) {
     const request = new XMLHttpRequest();
@@ -33,5 +35,90 @@ const getCountryData = function (country) {
   
   getCountryData('india');
   getCountryData('usa');
- 
+  getCountryData('germany');
+  getCountryData('france');
+ */
 
+///////////////////////////////////////
+// Welcome to Callback Hell
+/*
+const countriesContainer = document.querySelector('.countries');
+
+const getCountryData = function (country) {
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
+
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+
+    const html = `
+      <article class="country">
+        <img class="country__img" src="${data.flags['svg']}" />
+        <div class="country__data">
+          <h3 class="country__name">${data.name['common']}</h3>
+          <h4 class="country__region">${data.region}</h4>
+          <p class="country__row"><span>👫</span>${(
+            +data.population / 1000000
+          ).toFixed(1)} M people</p>
+          <p class="country__row"><span>🗣️</span>${
+            Object.values(data.languages)[0]
+          }</p>
+          <p class="country__row"><span>💰</span>${
+            Object.values(data.currencies)[0].name
+          }</p>
+        </div>
+      </article>
+    `;
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+
+    //////////////////request-2////////////////////
+    const request2 = new XMLHttpRequest();
+    request2.open(
+      'GET',
+      `https://restcountries.com/v3.1/name/${data.borders[0]}`
+    );
+    request2.send();
+    request2.addEventListener('load', function () {
+      const [data2] = JSON.parse(this.responseText);
+
+      const html = `
+        <article class="country">
+          <img class="country__img" src="${data2.flags['svg']}" />
+          <div class="country__data">
+            <h3 class="country__name">${data2.name['common']}</h3>
+            <h4 class="country__region">${data2.region}</h4>
+            <p class="country__row"><span>👫</span>${(
+              +data2.population / 1000000
+            ).toFixed(1)} M people</p>
+            <p class="country__row"><span>🗣️</span>${
+              Object.values(data2.languages)[0]
+            }</p>
+            <p class="country__row"><span>💰</span>${
+              Object.values(data2.currencies)[0].name
+            }</p>
+          </div>
+        </article>
+      `;
+      countriesContainer.insertAdjacentHTML('beforeend', html);
+      countriesContainer.style.opacity = 1;
+    });
+    ///////////////////////////////////////////
+  });
+};
+getCountryData('france');
+
+setTimeout(() => {
+  console.log('1 second passed');
+  setTimeout(() => {
+    console.log('2 seconds passed');
+    setTimeout(() => {
+      console.log('3 second passed');
+      setTimeout(() => {
+        console.log('4 second passed');
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}, 1000);
+*/
